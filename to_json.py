134681b04c0f5036a39e8683876e5bb1c2c05c00
Akshay Ajagekar <ajagekar.akshay@gmail.com>
2020-04-23 12:03:07 -0400

    Date update

--- to_json.py
+++ to_json.py
@@ -1,6 +1,6 @@
 import csv
 import json
-
+import numpy as np
 data = []
 
 source = "C:\\Users\\Akshay\\Documents\\GitHub\\covid-data\\ny cases by county - ls.csv"
@@ -10,8 +10,18 @@ with open(source) as csvfile:
     for rows in csv_reader:
         data.append(rows)
 
+for e in data:
+    for key in e:
+        try:
+            e[key] = int(e[key])
+        except:
+            try:
+                e[key] = np.round(float(e[key]), 2)
+            except:
+                pass
+
 target = "C:\\Users\\Akshay\\Documents\\GitHub\\covid-data\\data.js"
 
 with open(target, "w") as jsonfile:
-    jsonfile.write("data=")
-    json.dump(data, fp=jsonfile)
+    jsonfile.write("jsData=")
+    json.dump(data, fp=jsonfile, separators=(',', ':'))
