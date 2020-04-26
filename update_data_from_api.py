3b7678795381455822c1801f3eb4e435be058fc8
Akshay Ajagekar <ajagekar.akshay@gmail.com>
2020-04-25 21:46:05 -0400

    Date update

--- update_data_from_api.py
+++ update_data_from_api.py
@@ -12,44 +12,38 @@ def check_update():
     return results
 
 def update_data():
-    results = client.get(socrata_dataset_identifier, where="test_date > '"+last_updated+"'", select="test_date, county, new_positives, cumulative_number_of_positives", limit=70)
-
-    with open("dataJSON.json", 'r') as fp:
-        old_data = json.load(fp)
-
-    err = 0.01
-    data = {}
-
-    for row in results:
-        date_obj = datetime.datetime.strptime(row['test_date'], '%Y-%m-%dT00:00:00.000')
-        date_header = str(date_obj.month) + "/" + str(date_obj.day) + "/" + str(date_obj.year)
-
-        entry = {'county':row['county'], 
-                'cumcases':int(row['cumulative_number_of_positives'])}
-        try:
-            data[date_header].append(entry)
-        except KeyError:
-            data[date_header] = [entry]
+    results = client.get(socrata_dataset_identifier, where="test_date > '"+last_updated+"'", select="test_date, county, new_positives, cumulative_number_of_positives", limit=1000)
 
     row_src = []
     source = "ny cases by county - ls.csv"
     with open(source, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        headers = next(csvreader)
+    with open(source, 'r') as csvfile:
         csvreader = csv.DictReader(csvfile)
         for row in csvreader:
-            row_src.append(row)
-            for key in row_src[-1]:
+            for key in headers:
                 if "ls" in key:
-                    row_src[-1].pop(key)
+                    row.pop(key)
+            row_src.append(row)
 
+    for row in results:
+        date_obj = datetime.datetime.strptime(row['test_date'], '%Y-%m-%dT00:00:00.000')
+        date_header = str(date_obj.month) + "/" + str(date_obj.day) + "/" + str(date_obj.year)
+        for entry in row_src: 
+            if entry['county'].lower() == row['county'].lower():
+                entry[date_header] = row['cumulative_number_of_positives']
+    
 
-    for date in old_data:
-        data[date] = old_data[date]
+    target = source
+    with open(target, "w", newline="") as csvfile:
+        headers = list(row_src[0].keys())
+        writer = csv.DictWriter(csvfile, headers)
+        writer.writeheader()
+        for row in row_src:
+            writer.writerow(row)
 
-    target = "dataJSON.json"
-    with open(target, "w") as jsonfile:
-        json.dump(data, fp=jsonfile, separators=(',', ':'))
-
-
+# API config (Do not change)
 app_token = "1CKHfUB8qIpEQKUM1JNdiEK1N"
 socrata_dataset_identifier = "xdss-u53e"
 
@@ -57,17 +51,17 @@ client = Socrata("health.data.ny.gov", app_token)
 metadata = client.get_metadata(socrata_dataset_identifier)
 
 
-# today = datetime.date.today() -  datetime.timedelta(days=1)
-# today = datetime.datetime(today.year, today.month, today.day)
-# with open("Last updated.txt",'r') as fp:
-#     last_updated = fp.read()
-#     last_updated_obj = datetime.datetime.strptime(last_updated, '%Y-%m-%dT00:00:00.000')
+today = datetime.date.today() -  datetime.timedelta(days=1)
+today = datetime.datetime(today.year, today.month, today.day)
+with open("Last updated.txt",'r') as fp:
+    last_updated = fp.read()
+    last_updated_obj = datetime.datetime.strptime(last_updated, '%Y-%m-%dT00:00:00.000')
 
-# if today > last_updated_obj:
-#     if len(check_update()) > 0:
-#         update_data()
-#         print("Data files updated")
+if today > last_updated_obj:
+    if len(check_update()) > 0:
+        update_data()
+        print("Data files updated")
 
-#         with open("Last updated.txt", 'w') as fp:
-#             fp.write(today.strftime('%Y-%m-%dT00:00:00.000'))
+        with open("Last updated.txt", 'w') as fp:
+            fp.write(today.strftime('%Y-%m-%dT00:00:00.000'))
 
