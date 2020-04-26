945a07b639959bb98062376c393bcf0a1b7502d1
Akshay Ajagekar <ajagekar.akshay@gmail.com>
2020-04-25 21:03:07 -0400

    Date update

--- update_data_from_api.py
+++ update_data_from_api.py
@@ -34,28 +34,13 @@ def update_data():
     row_src = []
     source = "ny cases by county - ls.csv"
     with open(source, 'r') as csvfile:
-        csvreader = csv.reader(csvfile)
+        csvreader = csv.DictReader(csvfile)
         for row in csvreader:
             row_src.append(row)
+            for key in row_src[-1]:
+                if "ls" in key:
+                    row_src[-1].pop(key)
 
-    ids = {}
-    for i in range(1, len(row_src)):
-        try:
-            ids[row_src[i][1]].append(row_src[i][0])
-        except KeyError:
-            ids[row_src[i][1]] = [row_src[i][0]]
-
-    for date in data:
-        for i in range(len(data[date])):
-            cnt = data[date][i]
-            entry = {'id':ids[cnt['county']].pop(0),
-                    'county':cnt['county'],
-                    'newcases':cnt['newcases'], 
-                    'cumcases':cnt['cumcases'], 
-                    'ncls': cnt['ncls'],
-                    'ccls':cnt['ccls']  }
-            data[date][i] = entry
-            ids[cnt['county']].append( entry['id'] )
 
     for date in old_data:
         data[date] = old_data[date]
@@ -63,13 +48,6 @@ def update_data():
     target = "dataJSON.json"
     with open(target, "w") as jsonfile:
         json.dump(data, fp=jsonfile, separators=(',', ':'))
-
-    target_js = "data.js"
-    with open(target_js, "w") as jsfile:
-        jsfile.write("data=")
-        json.dump(data, fp=jsfile, separators=(',', ':'))
-        jsfile.write("\n"+"ny_pop=")
-        json.dump(ny_population, fp=jsfile, separators=(',', ':'))
 
 
 app_token = "1CKHfUB8qIpEQKUM1JNdiEK1N"
@@ -79,17 +57,17 @@ client = Socrata("health.data.ny.gov", app_token)
 metadata = client.get_metadata(socrata_dataset_identifier)
 
 
-today = datetime.date.today() -  datetime.timedelta(days=1)
-today = datetime.datetime(today.year, today.month, today.day)
-with open("Last updated.txt",'r') as fp:
-    last_updated = fp.read()
-    last_updated_obj = datetime.datetime.strptime(last_updated, '%Y-%m-%dT00:00:00.000')
+# today = datetime.date.today() -  datetime.timedelta(days=1)
+# today = datetime.datetime(today.year, today.month, today.day)
+# with open("Last updated.txt",'r') as fp:
+#     last_updated = fp.read()
+#     last_updated_obj = datetime.datetime.strptime(last_updated, '%Y-%m-%dT00:00:00.000')
 
-if today > last_updated_obj:
-    if len(check_update()) > 0:
-        update_data()
-        print("Data files updated")
+# if today > last_updated_obj:
+#     if len(check_update()) > 0:
+#         update_data()
+#         print("Data files updated")
 
-        with open("Last updated.txt", 'w') as fp:
-            fp.write(today.strftime('%Y-%m-%dT00:00:00.000'))
+#         with open("Last updated.txt", 'w') as fp:
+#             fp.write(today.strftime('%Y-%m-%dT00:00:00.000'))
 
